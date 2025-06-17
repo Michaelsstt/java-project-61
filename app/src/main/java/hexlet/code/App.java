3367e19a969a84bfa7fde20c213aa7e9691a5622
micha <michaetlt@gmail.com>
2025-06-17 17:53:38 +0300

    cd app && ./gradlew build sonar -info

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,39 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greetings();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean running = true;
+        while (running) {
+            printMenu();
+            System.out.print("Your choice: ");
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    Cli.greetings();
+                    break;
+                case "2":
+                    Even.run();
+                    break;
+                case "0":
+                    System.out.println("Goodbye!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
     }
 }
 
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,15 +3,19 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
+    private static String userName;
+
     public static void greetings() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Brain Games!");
-
         System.out.print("May I have your name? ");
-        String name = scanner.nextLine();
+        userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + "!");
+    }
 
-        System.out.println("Hello, " + name + "!");
+    public static String getUserName() {
+        return userName;
     }
 }
 
