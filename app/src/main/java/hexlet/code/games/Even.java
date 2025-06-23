3c345e1780df45f2c48897aafd01da11f8b4c116
micha <michaetlt@gmail.com>
2025-06-23 19:03:20 +0300

    Class Even final now, added constructor

---
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -0,0 +1,33 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Random;
+
+public final class Even {
+
+    private Even() {
+
+    }
+
+    private static final Random RANDOM = new Random();
+
+    public static void run() {
+        String[][] gameData = new String[Engine.ROUNDS_COUNT][2];
+
+        for (int i = 0; i < Engine.ROUNDS_COUNT; i++) {
+            int number = RANDOM.nextInt(100);
+            String question = Integer.toString(number);
+            String correctAnswer = isEven(number) ? "yes" : "no";
+
+            gameData[i][0] = question;
+            gameData[i][1] = correctAnswer;
+        }
+        Engine.runGame("Answer 'yes' if the number is even, otherwise answer 'no'.", gameData);
+
+    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
